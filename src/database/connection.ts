261f2cee2e714282ebb2bbe61d0a015397135d51
defauth98 <neto.daniribeiro@gmail.com>
2021-06-13 11:17:13 -0300

    arrumando o erro das entidades e adicionando o arquivo de teste

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,19 +1,17 @@
 import { createConnection } from 'typeorm';
 
-if (process.env.NODE_ENV !== 'test') {
-  createConnection({
-    entities: ['./src/entities/*.ts'],
-    type: 'postgres',
-    host: '172.19.0.2',
-    port: 5432,
-    username: 'postgres',
-    password: 'book',
-    database: 'books',
+createConnection({
+  entities: ['./src/entities/*.ts'],
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'books',
+})
+  .then((connection) => {
+    if (connection.isConnected === true) {
+      console.log('Banco de dados conectado');
+    }
   })
-    .then((connection) => {
-      if (connection.isConnected === true) {
-        console.log('Banco de dados conectado');
-      }
-    })
-    .catch((error) => console.error(error));
-}
+  .catch((error) => console.error(error));
