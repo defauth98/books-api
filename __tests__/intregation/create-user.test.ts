829069fba5582db94ef62ec98f8abff7b8304d75
defauth98 <neto.daniribeiro@gmail.com>
2021-06-18 12:04:54 -0300

    Refatorando os testes da criacao dos usuarios

--- __tests__/intregation/create-user.test.ts
+++ __tests__/intregation/create-user.test.ts
@@ -23,6 +23,10 @@ describe('User creation', () => {
     const createUserResponse = await request(app).post('/sign').send(user);
 
     expect(createUserResponse.status).toEqual(200);
+    expect(createUserResponse.body).toHaveProperty('user');
+    expect(createUserResponse.body).toHaveProperty('token');
+    expect(createUserResponse.body.user).toHaveProperty('id');
+    expect(createUserResponse.body.user).toHaveProperty('email');
   });
 
   it('not should be create a user without a name', async () => {
@@ -34,8 +38,8 @@ describe('User creation', () => {
       password: user.password,
     });
 
-    expect(createUserResponse.body.message).toEqual('Não foi informado o nome');
     expect(createUserResponse.status).toEqual(400);
+    expect(createUserResponse.body.name).toEqual(['name is a required field']);
   });
 
   it('not should be create a user without a email', async () => {
@@ -44,13 +48,11 @@ describe('User creation', () => {
     const createUserResponse = await request(app).post('/sign').send({
       name: user.name,
       email: '',
-      password: user.password,
+      password: user.password
     });
 
-    expect(createUserResponse.body.message).toEqual(
-      'Não foi informado o email'
-    );
     expect(createUserResponse.status).toEqual(400);
+    expect(createUserResponse.body.email).toEqual(['email must be at least 8 characters']);
   });
 
   it('not should be create a user without a password', async () => {
@@ -63,9 +65,7 @@ describe('User creation', () => {
     });
 
     expect(createUserResponse.status).toEqual(400);
-    expect(createUserResponse.body.message).toEqual(
-      'Não foi informado a senha'
-    );
+    expect(createUserResponse.body.password).toEqual(['password is a required field']);
   });
 
   it('should give a jwt token when accont has been created', async () => {
@@ -85,6 +85,7 @@ describe('User creation', () => {
     const secondRequest = await request(app).post('/sign').send(user);
 
     expect(secondRequest.body).toHaveProperty('error');
+    expect(secondRequest.body.error).toEqual('User has already been created');
     expect(secondRequest.status).toEqual(400);
   });
 });
