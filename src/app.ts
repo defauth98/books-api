9a53d7281ae707a456e543ba7b1ff31415fec919
defauth98 <neto.daniribeiro@gmail.com>
2021-07-10 10:31:03 -0300

    Adicionando o cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
-import './database/connection';
 import routes from './routes';
-
+import cors from 'cors';
 import 'dotenv/config';
+
+import './database/connection';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 
 export default app;
