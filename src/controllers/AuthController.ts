c612874369b0b7d39888c17d429da91aa0cb6a33
defauth98 <neto.daniribeiro@gmail.com>
2021-07-12 11:38:50 -0300

    fix: status code na tela de login

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -108,7 +108,7 @@ export default {
       .catch(err => {
         const error = {[err.path]:[...err.errors]}
 
-        return response.json(error);
+        return response.status(400).json({error});
       })
   },
 };
