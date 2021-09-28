1c82d8ec8eda4aacb45a60fc31c0bc7dee8c5b8e
defauth98 <neto.daniribeiro@gmail.com>
2021-09-28 17:05:58 -0300

    fix: conectando ao banco de dados do deploy

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,17 +1,31 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-  entities: ['./src/entities/*.ts'],
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'books',
-})
-  .then((connection) => {
-    if (connection.isConnected === true) {
-      console.log('Banco de dados conectado');
-    }
+if (process.env.PORT) {
+  createConnection({
+    entities: ['./src/entities/*.ts'],
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
   })
-  .catch((error) => console.error(error));
+    .then((connection) => {
+      if (connection.isConnected === true) {
+        console.log('Banco de dados conectado');
+      }
+    })
+    .catch((error) => console.error(error));
+} else {
+  createConnection({
+    entities: ['./src/entities/*.ts'],
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'books',
+  })
+    .then((connection) => {
+      if (connection.isConnected === true) {
+        console.log('Banco de dados conectado');
+      }
+    })
+    .catch((error) => console.error(error));
+}
