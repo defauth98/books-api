562a46792d5e4cc27370b4eb658948ce1c0f6b65
defauth98 <neto.daniribeiro@gmail.com>
2021-09-28 16:28:20 -0300

    feat: Configurações para o deploy no heroku

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ import app from './app';
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(process.env.NODE_PORT || 3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Servidor rodando em -> http://localhost:3333');
 });
