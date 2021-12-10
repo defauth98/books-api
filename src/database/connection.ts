992e42bc4616252d9d84dd113f7244d03d7e89bc
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-12-10 12:54:49 -0300

    fix postgress connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -19,19 +19,21 @@ if (process.env.PORT) {
     })
     .catch((error) => console.error(error));
 } else {
-  createConnection({
-    entities: ['./src/entities/*.ts'],
-    type: 'postgres',
-    host: 'postgres',
-    port: 5432,
-    username: 'postgres',
-    password: 'docker',
-    database: 'books',
-  })
-    .then((connection) => {
-      if (connection.isConnected === true) {
-        console.log('Banco de dados conectado');
-      }
+  if(process.env.NODE_ENV !== 'test') {
+    createConnection({
+      entities: ['./src/entities/*.ts'],
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'books',
     })
-    .catch((error) => console.error(error));
+      .then((connection) => {
+        if (connection.isConnected === true) {
+          console.log('Banco de dados conectado');
+        }
+      })
+      .catch((error) => console.error(error));
+  }
 }
